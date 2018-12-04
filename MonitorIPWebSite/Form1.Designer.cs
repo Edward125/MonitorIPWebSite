5dbdd1e596f3721684544aa2d2aa61ab21a77b1f
Edward <edward_song@yeah.net>
2018-12-04 09:29:48 +0800

    function ok,

--- MonitorIPWebSite/Form1.Designer.cs
+++ MonitorIPWebSite/Form1.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtIP1 = new System.Windows.Forms.TextBox();
-            this.txtWeb1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtWeb2 = new System.Windows.Forms.TextBox();
-            this.txtIP2 = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.txtWeb2 = new System.Windows.Forms.TextBox();
+            this.txtIP2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWeb1 = new System.Windows.Forms.TextBox();
+            this.txtIP1 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.lstInfo = new System.Windows.Forms.ListBox();
-            this.btnGo = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -57,56 +57,20 @@
             this.groupBox1.Controls.Add(this.txtIP1);
             this.groupBox1.Location = new System.Drawing.Point(24, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(436, 128);
+            this.groupBox1.Size = new System.Drawing.Size(467, 128);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
             // 
-            // txtIP1
+            // btnGo
             // 
-            this.txtIP1.Location = new System.Drawing.Point(77, 20);
-            this.txtIP1.Name = "txtIP1";
-            this.txtIP1.Size = new System.Drawing.Size(144, 21);
-            this.txtIP1.TabIndex = 0;
-            // 
-            // txtWeb1
-            // 
-            this.txtWeb1.Location = new System.Drawing.Point(77, 47);
-            this.txtWeb1.Name = "txtWeb1";
-            this.txtWeb1.Size = new System.Drawing.Size(283, 21);
-            this.txtWeb1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "IP地址1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 12);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "网址1";
-            // 
-            // txtWeb2
-            // 
-            this.txtWeb2.Location = new System.Drawing.Point(77, 101);
-            this.txtWeb2.Name = "txtWeb2";
-            this.txtWeb2.Size = new System.Drawing.Size(283, 21);
-            this.txtWeb2.TabIndex = 4;
-            // 
-            // txtIP2
-            // 
-            this.txtIP2.Location = new System.Drawing.Point(77, 74);
-            this.txtIP2.Name = "txtIP2";
-            this.txtIP2.Size = new System.Drawing.Size(144, 21);
-            this.txtIP2.TabIndex = 3;
+            this.btnGo.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(393, 23);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(68, 84);
+            this.btnGo.TabIndex = 7;
+            this.btnGo.Text = "Start";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
             // 
             // label3
             // 
@@ -126,39 +90,82 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "IP地址2";
             // 
+            // txtWeb2
+            // 
+            this.txtWeb2.Location = new System.Drawing.Point(77, 101);
+            this.txtWeb2.Name = "txtWeb2";
+            this.txtWeb2.Size = new System.Drawing.Size(296, 21);
+            this.txtWeb2.TabIndex = 4;
+            this.txtWeb2.Text = "http://111.9.31.168:8022/SN/";
+            // 
+            // txtIP2
+            // 
+            this.txtIP2.Location = new System.Drawing.Point(77, 74);
+            this.txtIP2.Name = "txtIP2";
+            this.txtIP2.Size = new System.Drawing.Size(144, 21);
+            this.txtIP2.TabIndex = 3;
+            this.txtIP2.Text = "111.9.31.168";
+            this.txtIP2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "网址1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP地址1";
+            // 
+            // txtWeb1
+            // 
+            this.txtWeb1.Location = new System.Drawing.Point(77, 47);
+            this.txtWeb1.Name = "txtWeb1";
+            this.txtWeb1.Size = new System.Drawing.Size(296, 21);
+            this.txtWeb1.TabIndex = 1;
+            this.txtWeb1.Text = "http://192.168.0.112:8022/SN/";
+            // 
+            // txtIP1
+            // 
+            this.txtIP1.Location = new System.Drawing.Point(77, 20);
+            this.txtIP1.Name = "txtIP1";
+            this.txtIP1.Size = new System.Drawing.Size(144, 21);
+            this.txtIP1.TabIndex = 0;
+            this.txtIP1.Text = "192.168.0.112";
+            this.txtIP1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.lstInfo);
             this.groupBox2.Location = new System.Drawing.Point(24, 146);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(436, 426);
+            this.groupBox2.Size = new System.Drawing.Size(475, 426);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
             // 
             // lstInfo
             // 
             this.lstInfo.FormattingEnabled = true;
+            this.lstInfo.HorizontalScrollbar = true;
             this.lstInfo.ItemHeight = 12;
             this.lstInfo.Location = new System.Drawing.Point(6, 20);
             this.lstInfo.Name = "lstInfo";
-            this.lstInfo.Size = new System.Drawing.Size(424, 400);
+            this.lstInfo.Size = new System.Drawing.Size(461, 400);
             this.lstInfo.TabIndex = 0;
-            // 
-            // btnGo
-            // 
-            this.btnGo.Location = new System.Drawing.Point(371, 23);
-            this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(52, 84);
-            this.btnGo.TabIndex = 7;
-            this.btnGo.Text = "开始";
-            this.btnGo.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(491, 587);
+            this.ClientSize = new System.Drawing.Size(510, 587);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
--- MonitorIPWebSite/Form1.cs
+++ MonitorIPWebSite/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Edward;
+using System.Net;
 
 namespace MonitorIPWebSite
 {
@@ -15,15 +17,162 @@ namespace MonitorIPWebSite
             InitializeComponent();
         }
 
+
+        #region 参数
+
+
+        public static bool IsRun = false;
+
+        #endregion
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Text = "Monitor IP & WebSite,Ver:" + Application.ProductVersion;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+
+            if (!IsRun)
+                btnGo.Text = "Stop";
+            else
+                btnGo.Text = "Start";
+
+            IsRun = !IsRun;
+
+            while (IsRun)
+            {
+                string result = "";
+                
+                if (Other.pingIp (txtIP1.Text.Trim ()))
+                    UpdateInfo (lstInfo,"Ping " +txtIP1.Text +" 成功.");
+                else
+                    UpdateInfo(lstInfo, "Ping " + txtIP1.Text + " 失败.");
+
+                if (Other.pingIp(txtIP2.Text.Trim()))
+                    UpdateInfo(lstInfo, "Ping " + txtIP2.Text + " 成功.");
+                else
+                    UpdateInfo(lstInfo, "Ping " + txtIP2.Text + " 失败.");
+
+                if (CheckUrlVisit(txtWeb1.Text.Trim(), out result))
+                    UpdateInfo(lstInfo, txtWeb1.Text + "访问成功.");
+                else
+                    UpdateInfo(lstInfo, txtWeb1.Text + "访问失败."+ result);
+
+                result = "";
+                if (CheckUrlVisit(txtWeb2.Text.Trim(), out result))
+                    UpdateInfo(lstInfo, txtWeb2.Text + "访问成功.");
+                else
+                    UpdateInfo(lstInfo, txtWeb2.Text + "访问失败." + result);
+
+
+
+
+
+                Delay(5000);
+
+            }
+       
+        }
+
+
+
+        
+
+
+
+
+
+        private void UpdateInfo(ListBox listbox, string message)
+        {
+            if (listbox.Items.Count > 1000)
+                listbox.Items.RemoveAt(0);
+
+            string item = string.Empty;
+            //listbox.Items.Add("");
+            item = DateTime.Now.ToString("HH:mm:ss") + " " + @message;
+            if (listbox.InvokeRequired)
+            {
+                listbox.BeginInvoke(new Action<string>((msg) =>
+                {
+                    listbox.Items.Add(msg);
+                }), item);
+
+            }
+            else
+            {
+                listbox.Items.Add(item);
+            }
+            if (listbox.Items.Count > 1)
+            {
+                listbox.TopIndex = listbox.Items.Count - 1;
+                listbox.SetSelected(listbox.Items.Count - 1, true);
+            }
+        
+        }
+
+        /// <summary>
+        /// 延時子程序
+        /// </summary>
+        /// <param name="interval">延時的時間，单位毫秒</param>
+        private void Delay(double interval)
+        {
+            DateTime time = DateTime.Now;
+            double span = interval * 10000;
+            while (DateTime.Now.Ticks - time.Ticks < span)
+            {
+                Application.DoEvents();
+            }
 
         }
 
 
 
+        public bool CheckUrlVisit(string url)
+        {
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                if (resp.StatusCode == HttpStatusCode.OK)
+                {
+                    resp.Close();
+                    return true;
+                }
+            }
+            catch (WebException webex)
+            {
+                return false;
+            }
+
+            return false;
+
+        }
 
 
+        public bool CheckUrlVisit(string url,out string result)
+        {
+            result = "OK";
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                if (resp.StatusCode == HttpStatusCode.OK)
+                {
+                    resp.Close();
+                    return true;
+                }
+            }
+            catch (WebException webex)
+            {
+                result = webex.Message;
+                return false;
+            }
+
+            return false;
+
+        }
 
     }
 }
