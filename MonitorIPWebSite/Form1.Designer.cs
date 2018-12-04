175aa3f792f8a9aa1e6bfa89c9a070b4f4b163a8
Edward <edward_song@yeah.net>
2018-12-04 09:07:39 +0800

    create

---
+++ MonitorIPWebSite/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace MonitorIPWebSite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtIP1 = new System.Windows.Forms.TextBox();
+            this.txtWeb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWeb2 = new System.Windows.Forms.TextBox();
+            this.txtIP2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstInfo = new System.Windows.Forms.ListBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnGo);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtWeb2);
+            this.groupBox1.Controls.Add(this.txtIP2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtWeb1);
+            this.groupBox1.Controls.Add(this.txtIP1);
+            this.groupBox1.Location = new System.Drawing.Point(24, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(436, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtIP1
+            // 
+            this.txtIP1.Location = new System.Drawing.Point(77, 20);
+            this.txtIP1.Name = "txtIP1";
+            this.txtIP1.Size = new System.Drawing.Size(144, 21);
+            this.txtIP1.TabIndex = 0;
+            // 
+            // txtWeb1
+            // 
+            this.txtWeb1.Location = new System.Drawing.Point(77, 47);
+            this.txtWeb1.Name = "txtWeb1";
+            this.txtWeb1.Size = new System.Drawing.Size(283, 21);
+            this.txtWeb1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP地址1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "网址1";
+            // 
+            // txtWeb2
+            // 
+            this.txtWeb2.Location = new System.Drawing.Point(77, 101);
+            this.txtWeb2.Name = "txtWeb2";
+            this.txtWeb2.Size = new System.Drawing.Size(283, 21);
+            this.txtWeb2.TabIndex = 4;
+            // 
+            // txtIP2
+            // 
+            this.txtIP2.Location = new System.Drawing.Point(77, 74);
+            this.txtIP2.Name = "txtIP2";
+            this.txtIP2.Size = new System.Drawing.Size(144, 21);
+            this.txtIP2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "网址2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "IP地址2";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstInfo);
+            this.groupBox2.Location = new System.Drawing.Point(24, 146);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(436, 426);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // lstInfo
+            // 
+            this.lstInfo.FormattingEnabled = true;
+            this.lstInfo.ItemHeight = 12;
+            this.lstInfo.Location = new System.Drawing.Point(6, 20);
+            this.lstInfo.Name = "lstInfo";
+            this.lstInfo.Size = new System.Drawing.Size(424, 400);
+            this.lstInfo.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(371, 23);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(52, 84);
+            this.btnGo.TabIndex = 7;
+            this.btnGo.Text = "开始";
+            this.btnGo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 587);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWeb2;
+        private System.Windows.Forms.TextBox txtIP2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWeb1;
+        private System.Windows.Forms.TextBox txtIP1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lstInfo;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
